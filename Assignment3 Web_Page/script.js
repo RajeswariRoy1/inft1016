a2a746b174c86b092db143fe7a3eb99434aaae50
Code520455 <130625580+RajeswariRoy1@users.noreply.github.com>
2023-05-14 12:05:23 +0930

    Update script.js

--- Assignment3 Web_Page/script.js
+++ Assignment3 Web_Page/script.js
@@ -1,7 +1,6 @@
 const notesContainer = document.querySelector(".notes-container")
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box")
-\
 createBtn.addEventListener('click', ()=>{
     let inputBox = document.createElement('p');
     let img = document.createElement('img');
@@ -9,6 +8,11 @@ createBtn.addEventListener('click', ()=>{
     inputBox.setAttribute('contenteditable', 'true');
     img.src= 'delete.png';
     notesContainer.appendChild(inputBox).appendChild(img);
-    
 
+})
+notesContainer.addEventListener('click',function(e){
+    if(e.target.tagName === 'IMG'){
+        e.target.parentElement.remove();
+        
+    }
 })
